db7a86e2730d3009bbcbc934e17712c67859a4b7
Misbah542 <misbahul8@gmail.com>
2026-04-19 10:37:59 +0530

    ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,14 +45,22 @@ const Navbar = () => {
         trigger: selector,
         start: 'top 80px',
         end: 'bottom 80px',
-        onEnter: () =>
-          document.querySelector('.header')?.classList.add('nav-on-light'),
-        onLeave: () =>
-          document.querySelector('.header')?.classList.remove('nav-on-light'),
-        onEnterBack: () =>
-          document.querySelector('.header')?.classList.add('nav-on-light'),
-        onLeaveBack: () =>
-          document.querySelector('.header')?.classList.remove('nav-on-light'),
+        onEnter: () => {
+          document.querySelector('.header')?.classList.add('nav-on-light');
+          document.querySelector('.icons-section')?.classList.add('nav-on-light');
+        },
+        onLeave: () => {
+          document.querySelector('.header')?.classList.remove('nav-on-light');
+          document.querySelector('.icons-section')?.classList.remove('nav-on-light');
+        },
+        onEnterBack: () => {
+          document.querySelector('.header')?.classList.add('nav-on-light');
+          document.querySelector('.icons-section')?.classList.add('nav-on-light');
+        },
+        onLeaveBack: () => {
+          document.querySelector('.header')?.classList.remove('nav-on-light');
+          document.querySelector('.icons-section')?.classList.remove('nav-on-light');
+        },
       });
       navTriggers.push(st);
     });
